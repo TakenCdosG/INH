0c350397acae68e253d237a8950b0cc477dbb97c
maicollopez <maicol.lopez@akendos.com>
2014-10-17 18:10:02 -0500

    Core Update, First Getlisted Functionality

--- sites/all/themes/newhaven/node--report_nightlife_page.tpl.php
+++ sites/all/themes/newhaven/node--report_nightlife_page.tpl.php
@@ -5,7 +5,7 @@
   <div class="content">
 	  <?php
 		print $node->body["und"][0]["safe_value"];
-		print views_embed_view("report_nightlife", "page",$_GET['s']); 
+		print views_embed_view("report_nightlife", "page",$_GET['s']);
 		?>
 
   </div>
