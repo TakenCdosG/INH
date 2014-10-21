d59d107f696aaec2f0480dad3d9bee111498bd14
maicollopez <maicol.lopez@akendos.com>
2014-10-21 16:00:31 -0500

    Styles administer forms

--- sites/all/themes/newhaven/node--report_services_page.tpl.php
+++ sites/all/themes/newhaven/node--report_services_page.tpl.php
@@ -45,30 +45,4 @@
 	  </div>
 	  <div class='img-bottom'></div>
 	</div>
-
-
-	<!--<div id="content-shopping-right"  style="width: 265px;margin-top:20px;">
-
-		<div class="region region-sidebar-right">
-			<div class="widget" style="height: 77px;">
-
-		  <div class="content">
-			<div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><div><a class="bucket" href="/culture-listing"> <span class="icon"> <img src="/sites/default/files/arts_0.png" alt="Culture" width="37" height="38"></span> Culture </a></div></div></div></div>  </div>
-
-		</div>
-		<div class="widget" style="height: 77px;">
-
-		  <div class="content">
-			<div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><div><a class="bucket" href="/dining-listing"> <span class="icon"> <img src="/sites/default/files/dining_0.png" alt="Dining" width="38" height="50"></span> Dining </a></div></div></div></div>  </div>
-
-		</div>
-		<div class="widget" style="height: 77px;">
-
-		  <div class="content">
-			<div class="field field-name-body field-type-text-with-summary field-label-hidden"><div class="field-items"><div class="field-item even" property="content:encoded"><div><a class="bucket" href="/shopping-listing"> <span class="icon"> <img src="/sites/default/files/shopping_0.png" alt="Shopping" width="33" height="38"></span> Shopping </a></div></div></div></div>  </div>
-
-		</div>
-	   </div>
-
-	</div>-->
 </div>
