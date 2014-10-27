d2a2be0092b8066313111613e51df596646686f3
maicollopez <maicol.lopez@akendos.com>
2014-10-27 15:37:18 -0500

    Dining Forms Issues

--- sites/all/modules/custom_form/js/custom_form.js
+++ sites/all/modules/custom_form/js/custom_form.js
@@ -18,9 +18,9 @@ jQuery().ready(function() {
 		  }
 		});
 		jQuery('#edit-field-category-services-und option[value=_none]"').remove();
-		
+
 	}
-	
+
 	/******************DAY AND HOURS**************************************************************************************/
 	jQuery('#markup_day_hour .form-checkbox').change(function() {
 	 var id = jQuery(this).attr("id");
@@ -32,9 +32,9 @@ jQuery().ready(function() {
 	    jQuery("#"+id+"_text").css('background-color','#E0E0D8');
 		 jQuery("#"+id+"_text").val('');
 	   }
-	  
+
 	});
-   
+
     jQuery("#edit-field-days-and-hours-und-0-field-day-of-operation-und  option[value='sunday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-hours-und-1-field-day-of-operation-und  option[value='monday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-hours-und-2-field-day-of-operation-und  option[value='tuesday']").attr("selected","selected");
@@ -42,12 +42,12 @@ jQuery().ready(function() {
 	jQuery("#edit-field-days-and-hours-und-4-field-day-of-operation-und  option[value='thursday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-hours-und-5-field-day-of-operation-und  option[value='friday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-hours-und-6-field-day-of-operation-und  option[value='saturday']").attr("selected","selected");
-	
+
 	/*************************************************************************************************************************/
-	
-	
-	
-	
+
+
+
+
 	/******************DAYS AND MEAlS SCHEDULES**************************************************************************************/
 	jQuery('#markup_day_hschedules .form-checkbox').change(function() {
 	 var id = jQuery(this).attr("id");
@@ -64,9 +64,9 @@ jQuery().ready(function() {
 		jQuery("#"+id+"_lunch").val('');
 		jQuery("#"+id+"_dinner").val('');
 	   }
-	  
+
 	});
-   
+
     jQuery("#edit-field-days-and-meals-schedules-und-0-field-day-of-operation-und  option[value='sunday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-meals-schedules-und-1-field-day-of-operation-und  option[value='monday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-meals-schedules-und-2-field-day-of-operation-und  option[value='tuesday']").attr("selected","selected");
@@ -74,68 +74,68 @@ jQuery().ready(function() {
 	jQuery("#edit-field-days-and-meals-schedules-und-4-field-day-of-operation-und  option[value='thursday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-meals-schedules-und-5-field-day-of-operation-und  option[value='friday']").attr("selected","selected");
 	jQuery("#edit-field-days-and-meals-schedules-und-6-field-day-of-operation-und  option[value='saturday']").attr("selected","selected");
-	
+
 	/*************************************************************************************************************************/
-	
+
 
    jQuery('#dining-node-form').submit(function() {
    /******************DAY AND HOURS**************************************************************************************/
    checkbox_field();
 	 /*************************************************************************************************************************/
-	 
+
 	  /******************DAYS AND MEAlS SCHEDULES**************************************************************************************/
-	 if (jQuery("#marker_hschedules_sunday").attr('checked')) { 
+	 if (jQuery("#marker_hschedules_sunday").attr('checked')) {
      jQuery("#edit-field-days-and-meals-schedules-und-0-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_sunday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-0-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_sunday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-0-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_monday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_monday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-1-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_monday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-1-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_monday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-1-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_tuesday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_tuesday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-2-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_tuesday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-2-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_tuesday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-2-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_wednesday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_wednesday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-3-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_wednesday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-3-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_wednesday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-3-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_thursday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_thursday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-4-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_thursday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-4-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_thursday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-4-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_friday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_friday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-5-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_friday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-5-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_friday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-5-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
-	 if (jQuery("#marker_hschedules_saturday").attr('checked')) { 
+
+	 if (jQuery("#marker_hschedules_saturday").attr('checked')) {
 	 jQuery("#edit-field-days-and-meals-schedules-und-6-field-lunch-hours-und-0-value").val(jQuery("#marker_hschedules_saturday_lunch").val());
 	 jQuery("#edit-field-days-and-meals-schedules-und-6-field-dinner-hours-und-0-value").val(jQuery("#marker_hschedules_saturday_dinner").val());
 	 }else{
 	  jQuery("#edit-field-days-and-meals-schedules-und-6-field-day-of-operation-und  option[value='_none']").attr("selected","selected");
 	 }
-	 
+
 	 /*************************************************************************************************************************/
-	    
-		  
+
+
 	});
 	
 	jQuery('#shopping-node-form').submit(function() {
